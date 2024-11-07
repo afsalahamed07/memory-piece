c997237b93e4106c2718da5de242d63af66d5d3a
agrippa <afsalahamed07@gmail.com>
2024-11-07 14:54:11 +0530

    some configuration for ts

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
 
 export default [
   { ignores: ["dist"] },
@@ -11,10 +13,12 @@ export default [
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: typescriptParser,
       parserOptions: {
         ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
         sourceType: "module",
+        project: "./tsconfig.json", // Ensures TypeScript rules have project context
       },
     },
     settings: { react: { version: "18.3" } },
@@ -22,12 +26,14 @@ export default [
       react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "@typescript-eslint": typescriptEslint,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
+      ...typescriptEslint.configs.recommended.rules, // Adds TypeScript rules
       "react/jsx-no-target-blank": "off",
       "react-refresh/only-export-components": [
         "warn",
